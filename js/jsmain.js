fb2ba72632f3c50ad6779b5f57a1302894aa6feb
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-01-24 02:04:13 +0200

    Completed Even or odd Challenge.

--- js/jsmain.js
+++ js/jsmain.js
@@ -3,11 +3,18 @@ function myFunction() {
     let htmlnum = document.getElementById('numInput').value;
 
 
-    /*by using isNaN (is Not a Number) we will define if input is a number or not*/
+    // If the variable is not a number we want to display an alert.
     if (isNaN(htmlnum)) {
         alert(htmlnum + "is not a number ! <br>");
-    } else {
-        document.write(htmlnum + "is a number ! <br>")
+    } 
+    //If variable is a number we want to know if the number is even or odd.
+    /*Checking if number is even*/
+    if(htmlnum % 2 == 0){
+        document.write(htmlnum + " is an even number.")
+    }
+    //if the number is not even
+    else{
+        document.write(htmlnum + " is an odd number.")
     }
 
 
