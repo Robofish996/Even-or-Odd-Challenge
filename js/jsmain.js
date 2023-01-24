36d3e4303195e7597a8a5c085dce14bc2613a406
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-01-24 02:13:47 +0200

    Finished Even or odd challenge

--- js/jsmain.js
+++ js/jsmain.js
@@ -6,37 +6,15 @@ function myFunction() {
     // If the variable is not a number we want to display an alert.
     if (isNaN(htmlnum)) {
         alert(htmlnum + "is not a number ! <br>");
-    } 
+    }
     //If variable is a number we want to know if the number is even or odd.
     /*Checking if number is even*/
-    if(htmlnum % 2 == 0){
-        document.write(htmlnum + " is an even number.")
+    if (htmlnum % 2 == 0) {
+        document.write(htmlnum + " is an even number.");
     }
     //if the number is not even
-    else{
-        document.write(htmlnum + " is an odd number.")
+    else {
+        document.write(htmlnum + " is an odd number.");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 } 
